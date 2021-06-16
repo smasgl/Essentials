747696b9cce75c9758234efcab6612b352a4480f
lwnmas9 <masooglu.simon@gmail.com>
2021-06-16 13:53:02 +0200

    Init

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
+[![Version: 1.0 Release](https://shields.io/badge/Version-1.0%20Release-green.svg)](https://github.com/smasgl/Essentials)
+[![License: GPL v3](https://shields.io/badge/License-GPL%20v3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+
 # Essentials
+
+## Description
+
 Essential methods 4 programming.
---
+++ Types/Integer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Essentials.Types
+{
+    public static class Integer
+    {
+        public static int ToInt(this string text)
+        {
+            if (int.TryParse(text, out int integer))
+                throw new FormatException($"Could not convert {text} to integer");
+            return integer;
+        }
+    }
+}
